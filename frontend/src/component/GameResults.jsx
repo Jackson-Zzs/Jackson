bb8380d91f038aa454772abc0ab7d19dd60ea280
Lachlan Sutton <l.sutton@student.unsw.edu.au>
2023-04-15 21:35:23 +1000

    Finish up 2.3 (probably, it's hard to test)

--- frontend/src/component/GameResults.jsx
+++ frontend/src/component/GameResults.jsx
@@ -1,7 +1,27 @@
 // GameResults.jsx
 import React, { useEffect, useState } from 'react';
-import { Button, Space } from 'antd';
+import { Button, Space, Table } from 'antd';
 import { useParams } from 'react-router-dom';
+
+import {
+  CategoryScale,
+  Chart,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 function GameResults ({ token }) {
   // Don't do anything unless we're logged in
@@ -11,10 +31,112 @@ function GameResults ({ token }) {
 
   const { sessionid } = useParams();
   const [session, setSession] = useState(null);
+  const [quiz, setQuiz] = useState(null);
   const [results, setResults] = useState(null);
 
+  async function fetchSession () {
+    const response = await fetch(`http://localhost:5005/admin/session/${sessionid}/status`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    setSession(data);
+  }
+
+  async function fetchQuiz () {
+    const response = await fetch('http://localhost:5005/admin/quiz', {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+
+    const sessionIdNum = parseInt(sessionid);
+    const candidates = data.quizzes.filter(testQuiz => testQuiz.active === sessionIdNum || testQuiz.oldSessions.includes(sessionIdNum));
+
+    if (candidates.length <= 0) {
+      setQuiz(null);
+      return;
+    }
+
+    const quizResponse = await fetch(`http://localhost:5005/admin/quiz/${candidates[0].id}`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const quizData = await quizResponse.json();
+
+    const fullQuiz = { ...quizData, id: candidates[0].id }
+    setQuiz(fullQuiz);
+  }
+
+  const calculateScores = () => {
+    /*
+      Sorted by score
+      percentage per question
+      average time per question
+    */
+
+    const numQuestions = quiz.questions.length;
+
+    const userScores = new Map();
+    const numCorrect = new Array(numQuestions).fill(0);
+    const totalTime = new Array(numQuestions).fill(0);
+    const numAnswers = new Array(numQuestions).fill(0);
+
+    results.results.forEach((user) => {
+      let totalScore = 0;
+
+      user.answers.forEach((answer, index) => {
+        if (answer.correct) {
+          totalScore += quiz.questions[index].points;
+
+          numCorrect[index]++;
+        }
+
+        const startTime = Date.parse(answer.questionStartedAt);
+        const answerTime = Date.parse(answer.answeredAt);
+
+        totalTime[index] += answerTime - startTime;
+
+        numAnswers[index]++;
+      });
+
+      userScores.set(user.name, totalScore);
+    });
+
+    const percentages = numCorrect.map((num, index) => {
+      if (numAnswers[index] === 0) {
+        return 1.0;
+      }
+
+      return num / numAnswers[index];
+    });
+    const averageTime = totalTime.map((time, index) => {
+      if (numAnswers[index] === 0) {
+        return 0.0;
+      }
+
+      return time / numAnswers[index];
+    });
+
+    return {
+      userScores,
+      percentages,
+      averageTime,
+    };
+  }
+
   async function fetchResults () {
-    if (!session) {
+    // Leave if results aren't valid
+    if (!session || session.results.active) {
       return;
     }
 
@@ -29,44 +151,144 @@ function GameResults ({ token }) {
     setResults(data);
   }
 
-  async function fetchSession () {
-    const response = await fetch(`http://localhost:5005/admin/session/${sessionid}/status`, {
-      method: 'GET',
+  async function advanceSession () {
+    await fetch(`http://localhost:5005/admin/quiz/${quiz.id}/advance`, {
+      method: 'POST',
       headers: {
         'Content-type': 'application/json',
         Authorization: `Bearer ${token}`,
       },
     });
-    const data = await response.json();
-    setSession(data);
+    fetchSession();
   }
 
-  useEffect(fetchSession, [sessionid, token]);
+  async function endSession () {
+    await fetch(`http://localhost:5005/admin/quiz/${quiz.id}/end`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    fetchSession();
+  }
+
+  useEffect(() => {
+    fetchSession();
+    fetchQuiz();
+  }, [sessionid, token]);
   useEffect(fetchResults, [session]);
 
-  if (!session) {
+  if (!session || !quiz) {
     return <div>Loading...</div>;
   }
 
   if (session.results.active) {
     return (
     <div>
-      <Space direction='horizontal'>
+      <Space direction='horizontal' onClick={advanceSession}>
         <Button type='primary'>
-          Advance
+          Advance ({session.results.position})
         </Button>
 
-        <Button danger type='primary'>
+        <Button danger type='primary' onClick={endSession}>
           Stop
         </Button>
       </Space>
     </div>);
   } else {
-    console.log(results);
+    if (!results) {
+      return <div>Loading results...</div>;
+    }
+
+    const scoreData = calculateScores();
+    console.log(scoreData);
+
+    const options = {
+      responsive: true,
+      interaction: {
+        mode: 'index',
+        intersect: false,
+      },
+      stacked: false,
+      plugins: {
+        title: {
+          display: true,
+          text: `Game results - ${quiz.name}`,
+        },
+      },
+      scales: {
+        perc: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+          min: 0.0,
+          max: 1.0,
+        },
+        time: {
+          type: 'linear',
+          display: true,
+          position: 'right',
+          grid: {
+            drawOnChartArea: false,
+          },
+          min: 0.0,
+        },
+      },
+    };
+
+    const labels = Array.from({ length: scoreData.percentages.length }, (_, i) => `Question ${i + 1}`);
+
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: 'Percentages',
+          data: scoreData.percentages,
+          borderColor: 'rgb(255, 99, 132)',
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          yAxisID: 'perc',
+        },
+        {
+          label: 'Average Time',
+          data: scoreData.averageTime,
+          borderColor: 'rgb(53, 162, 235)',
+          backgroundColor: 'rgba(53, 162, 235, 0.5)',
+          yAxisID: 'time',
+        }
+      ]
+    };
+
+    const userScores = Array.from(scoreData.userScores, ([username, score]) => ({ username, score }))
+      .sort(({ username, score }) => score);
+
+    const topUsers = userScores.slice(0, 5);
+
+    const tableData = topUsers.map(({ username, score }, index) => {
+      return {
+        key: index,
+        username,
+        score
+      }
+    })
+
+    const columns = [
+      {
+        title: 'Name',
+        dataIndex: 'username',
+        key: 'username'
+      },
+      {
+        title: 'Score',
+        dataIndex: 'score',
+        key: 'score'
+      }
+    ]
 
     return (
     <div>
-        NOT ACTIVE
+        <Bar options={options} data={data} />
+        <Table dataSource={tableData} columns={columns} />
     </div>
     );
   }
